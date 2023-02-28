aefacdf0e46f425971a19b564e3f0cc859724f27
adelhassan6644 <110145987+adelhassan6644@users.noreply.github.com>
2023-02-28 22:27:43 +0200

    Redesign Ui add Request

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:hr_project/navigation/custom_navigation.dart';
 import 'package:hr_project/presentation/auth/login_screen.dart';
 import 'package:hr_project/presentation/notifier/localization_provider.dart';
-import 'package:hr_project/main_provider.dart';
 import 'package:hr_project/presentation/notifier/theme_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:upgrader/upgrader.dart';
@@ -17,6 +16,12 @@ import 'package:flutter/services.dart';
 import 'app/core/utils/app_strings.dart';
 import 'domain/localization/app_localization.dart';
 import 'navigation/routes.dart';
+import 'presentation/notifier/attendance_schedules_provider.dart';
+import 'presentation/notifier/add_request_provider.dart';
+import 'presentation/notifier/attendance_repo_provider.dart';
+import 'presentation/notifier/auth_provider.dart';
+import 'presentation/notifier/language_provider.dart';
+import 'presentation/notifier/profile_provider.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,7 +35,17 @@ Future<void> main() async {
   }
 
   await di.init();
-  runApp(MultiProvider(providers: MainProvider.providers, child: const MyApp()));
+  runApp(MultiProvider(providers:[
+    ChangeNotifierProvider(create: (context) => di.sl<ThemeProvider>()),
+    ChangeNotifierProvider(create: (context) => di.sl<AddRequestProvider>()),
+    ChangeNotifierProvider(create: (context) => di.sl<AuthProvider>()),
+    ChangeNotifierProvider(create: (context) => di.sl<ProfileProvider>()),
+    ChangeNotifierProvider(create: (context) => di.sl<LanguageProvider>()),
+    ChangeNotifierProvider(create: (context) => di.sl<LocalizationProvider>()),
+    ChangeNotifierProvider(create: (context) => di.sl<AttendanceProvider>()),
+    ChangeNotifierProvider(create: (context) => di.sl<AttendanceScheduleProvider>()),
+
+  ], child: const MyApp()));
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
       statusBarBrightness: Brightness.light,
       statusBarColor: Colors.transparent));
