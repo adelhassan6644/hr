6b33cb93f926a413f9e6faf0058482f9b9199013
Adel Hassan <110145987+adelhassan6644@users.noreply.github.com>
2023-09-26 16:28:04 +0300

    dtu

--- lib/app/localization/language_constant.dart
+++ lib/app/localization/language_constant.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hr_project/navigation/custom_navigation.dart';
 import 'app_localization.dart';
 
-
 String getTranslated(String key, BuildContext context) {
-  return AppLocalization.of(context)!.translate(key);
+  return AppLocalization.of(CustomNavigator.navigatorState.currentContext!)!
+      .translate(key);
 }
