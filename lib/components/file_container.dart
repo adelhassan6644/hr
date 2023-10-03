becf42038cefb3622f24ac5457970331f8254265
Adel Hassan <110145987+adelhassan6644@users.noreply.github.com>
2023-10-03 16:31:42 +0300

    update main

--- lib/components/file_container.dart
+++ lib/components/file_container.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:open_file_plus/open_file_plus.dart';
 import 'package:yusrPlus/app/core/extensions.dart';
 import '../app/core/color_resources.dart';
 import '../app/core/svg_images.dart';
@@ -9,7 +10,7 @@ import '../helpers/file_picker_helper.dart';
 import 'custom_images.dart';
 
 class FileContainer extends StatelessWidget {
-  const FileContainer({Key? key, this.attachments,required this.onRemove})
+  const FileContainer({Key? key, this.attachments, required this.onRemove})
       : super(key: key);
   final List<File>? attachments;
   final Function(List<File>?) onRemove;
@@ -30,62 +31,71 @@ class FileContainer extends StatelessWidget {
             attachments?.length ?? 0,
             (index) => Column(
                   children: [
-                    Row(
-                      children: [
-                        Container(
-                          height: 48,
-                          width: 48,
-                          padding: const EdgeInsets.all(14),
-                          decoration: BoxDecoration(
-                              color: Styles.WHITE,
-                              borderRadius: BorderRadius.circular(12)),
-                          child: customImageIconSVG(
-                            imageName: SvgImages.documentFile,
-                            height: 24,
-                            width: 24,
+                    GestureDetector(
+                      onTap: () => OpenFile.open(attachments![index].path),
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 48,
+                            width: 48,
+                            padding: const EdgeInsets.all(14),
+                            decoration: BoxDecoration(
+                                color: Styles.WHITE,
+                                borderRadius: BorderRadius.circular(12)),
+                            child: customImageIconSVG(
+                              imageName: SvgImages.documentFile,
+                              height: 24,
+                              width: 24,
+                            ),
                           ),
-                        ),
-                        SizedBox(
-                          width: 12.w,
-                        ),
-                        Expanded(
-                            child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Text(
-                              FilePickerHelper.getName(
-                                  attachments![index].path),
-                              style: AppTextStyles.w700
-                                  .copyWith(color: Styles.HEADER, fontSize: 12),
-                            ),
-                            SizedBox(
-                              height: 8.h,
-                            ),
-                            FutureBuilder(
-                              future: FilePickerHelper.getFileSize(
-                                  attachments![index], 1),
-                              builder: (BuildContext context,
-                                  AsyncSnapshot<dynamic> snapshot) {
-                                return Text(
-                                  snapshot.hasData ? "${snapshot.data}" : "",
-                                  style: AppTextStyles.w700.copyWith(
-                                      color: Styles.HEADER.withOpacity(0.4),
-                                      fontSize: 10),
-                                );
-                              },
-                            )
-                          ],
-                        )),
-                        customImageIconSVG(
-                            imageName: SvgImages.trash,
-                            height: 20,
-                            width: 20,
-                            onTap: () {
-                              attachments!.removeAt(index);
-                              onRemove(attachments);
-                            }),
-                      ],
+                          SizedBox(
+                            width: 12.w,
+                          ),
+                          Expanded(
+                              child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                FilePickerHelper.getName(
+                                    attachments![index].path),
+                                maxLines: 2,
+                                style: AppTextStyles.w700.copyWith(
+                                    overflow: TextOverflow.ellipsis,
+                                    color: Styles.HEADER,
+                                    fontSize: 12),
+                              ),
+                              SizedBox(
+                                height: 8.h,
+                              ),
+                              FutureBuilder(
+                                future: FilePickerHelper.getFileSize(
+                                    attachments![index], 1),
+                                builder: (BuildContext context,
+                                    AsyncSnapshot<dynamic> snapshot) {
+                                  return Text(
+                                    snapshot.hasData ? "${snapshot.data}" : "",
+                                    style: AppTextStyles.w700.copyWith(
+                                        color: Styles.HEADER.withOpacity(0.4),
+                                        fontSize: 10),
+                                  );
+                                },
+                              )
+                            ],
+                          )),
+                          SizedBox(
+                            width: 12.w,
+                          ),
+                          customImageIconSVG(
+                              imageName: SvgImages.trash,
+                              height: 20,
+                              width: 20,
+                              onTap: () {
+                                attachments!.removeAt(index);
+                                onRemove(attachments);
+                              }),
+                        ],
+                      ),
                     ),
                     Visibility(
                         visible: index != attachments!.length - 1,
