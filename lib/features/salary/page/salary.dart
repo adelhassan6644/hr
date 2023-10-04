795ddf689dec1e290da05b1e10f03e213f4c0e2f
Adel Hassan <110145987+adelhassan6644@users.noreply.github.com>
2023-10-04 17:49:17 +0300

    update main

--- lib/features/salary/page/salary.dart
+++ lib/features/salary/page/salary.dart
@@ -9,190 +9,203 @@ import '../../../app/core/dimensions.dart';
 import '../../../app/localization/language_constant.dart';
 import '../../../components/custom_app_bar.dart';
 import '../../../components/custom_button.dart';
+import '../../../components/loader_view.dart';
 import '../../../main_providers/download_provider.dart';
 import '../../../navigation/routes.dart';
+import '../provider/salary_provider.dart';
 
 class Salary extends StatelessWidget {
   const Salary({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(
-        title: getTranslated("salary_&_financial", context),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: ListAnimator(
-              customPadding: EdgeInsets.symmetric(
-                  horizontal: Dimensions.PADDING_SIZE_DEFAULT.w,
-                  vertical: Dimensions.PADDING_SIZE_DEFAULT.h),
-              data: [
-                Container(
-                  padding:
-                      const EdgeInsets.all(Dimensions.PADDING_SIZE_DEFAULT),
-                  decoration: const BoxDecoration(
-                    color: Styles.FILL_COLOR,
-                    borderRadius: BorderRadius.all(Radius.circular(15)),
-                  ),
-                  child: Row(
-                    children: [
-                      CircularPercentIndicator(
-                        radius: 70.0,
-                        lineWidth: 12.0,
-                        percent: 0.2,
-                        center: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            const Text(
-                              "10217.50",
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Styles.blackColor,
-                                fontWeight: FontWeight.w600,
-                              ),
+    return SafeArea(
+      child: Scaffold(
+        appBar: CustomAppBar(
+          title: getTranslated("salary_&_financial", context),
+        ),
+        body: Consumer<SalaryProvider>(builder: (_, provider, child) {
+          return provider.isLoading
+              ? const LoaderView()
+              : Column(
+                  children: [
+                    Expanded(
+                      child: ListAnimator(
+                        customPadding: EdgeInsets.symmetric(
+                            horizontal: Dimensions.PADDING_SIZE_DEFAULT.w,
+                            vertical: Dimensions.PADDING_SIZE_DEFAULT.h),
+                        data: [
+                          Container(
+                            padding: const EdgeInsets.all(
+                                Dimensions.PADDING_SIZE_DEFAULT),
+                            decoration: const BoxDecoration(
+                              color: Styles.FILL_COLOR,
+                              borderRadius: BorderRadius.all(Radius.circular(15)),
                             ),
-                            Text(
-                              getTranslated("sar", context),
-                              style: const TextStyle(
-                                fontSize: 16,
-                                color: Styles.blackColor,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                          ],
-                        ),
-                        progressColor: Styles.PRIMARY_COLOR,
-                        backgroundColor: Styles.GOLD_COLOR,
-                      ),
-                      const SizedBox(
-                        width: 20,
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            getTranslated("base_salary", context),
-                            style: const TextStyle(
-                                fontSize: 14,
-                                color: Styles.PRIMARY_COLOR,
-                                fontWeight: FontWeight.w500),
-                          ),
-                          const Text(
-                            "5675.8",
-                            style: TextStyle(
-                                fontSize: 14,
-                                color: Styles.grayColor,
-                                fontWeight: FontWeight.w500),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            getTranslated("health_insurance", context),
-                            style: const TextStyle(
-                                fontSize: 14,
-                                color: Styles.GOLD_COLOR,
-                                fontWeight: FontWeight.w500),
-                          ),
-                          const Text(
-                            "534.34",
-                            style: TextStyle(
-                                fontSize: 14,
-                                color: Styles.grayColor,
-                                fontWeight: FontWeight.w500),
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            getTranslated("family_allowance", context),
-                            style: const TextStyle(
-                                fontSize: 14,
-                                color: Styles.GOLD_COLOR,
-                                fontWeight: FontWeight.w500),
-                          ),
-                          const Text(
-                            "5675.8",
-                            style: TextStyle(
-                                fontSize: 14,
-                                color: Styles.grayColor,
-                                fontWeight: FontWeight.w500),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 24.h,
-                ),
-                Text(getTranslated("salary_details", context),
-                    style: const TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 16,
-                    )),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 16.h),
-                  child: Container(
-                      padding: const EdgeInsets.all(
-                          Dimensions.PADDING_SIZE_EXTRA_LARGE),
-                      decoration: const BoxDecoration(
-                        color: Styles.FILL_COLOR,
-                        borderRadius: BorderRadius.all(Radius.circular(15)),
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Text(
-                            DateTime.now().monthFormat(),
-                            style: const TextStyle(
-                                fontSize: 14,
-                                color: Styles.blackColor,
-                                fontWeight: FontWeight.w600),
-                          ),
-
-                          ///Open Invoice
-                          Padding(
-                            padding: EdgeInsets.symmetric(
-                                vertical: 12.h,
-                                horizontal: Dimensions.PADDING_SIZE_DEFAULT.w),
-                            child: CustomButton(
-                              onTap: () => CustomNavigator.push(Routes.PDF,
-                                  arguments: ""),
-                              text: getTranslated("details", context),
-                            ),
-                          ),
-                          ChangeNotifierProvider(
-                            create: (_) => DownloadProvider(),
-                            child: Consumer<DownloadProvider>(
-                                builder: (_, provider, child) {
-                              return CustomButton(
-                                  textColor: Styles.PRIMARY_COLOR,
-                                  text: getTranslated("download", context),
-                                  onTap: () async {
-                                    if (!provider.downloaded) {
-                                      provider.download("", "".split("/").last);
-                                    }
-                                  },
-                                  isLoading: provider.isLoading,
-                                  lIconWidget: const Icon(
-                                    Icons.download,
-                                    color: Styles.PRIMARY_COLOR,
+                            child: Row(
+                              children: [
+                                CircularPercentIndicator(
+                                  radius: 70.0,
+                                  lineWidth: 12.0,
+                                  percent: 0.2,
+                                  center: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: [
+                                      const Text(
+                                        "10217.50",
+                                        style: TextStyle(
+                                          fontSize: 16,
+                                          color: Styles.blackColor,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                      Text(
+                                        getTranslated("sar", context),
+                                        style: const TextStyle(
+                                          fontSize: 16,
+                                          color: Styles.blackColor,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                    ],
                                   ),
-                                  backgroundColor: Styles.WHITE);
-                            }),
+                                  progressColor: Styles.PRIMARY_COLOR,
+                                  backgroundColor: Styles.GOLD_COLOR,
+                                ),
+                                const SizedBox(
+                                  width: 20,
+                                ),
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      getTranslated("base_salary", context),
+                                      style: const TextStyle(
+                                          fontSize: 14,
+                                          color: Styles.PRIMARY_COLOR,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    const Text(
+                                      "5675.8",
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          color: Styles.grayColor,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      getTranslated("health_insurance", context),
+                                      style: const TextStyle(
+                                          fontSize: 14,
+                                          color: Styles.GOLD_COLOR,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    const Text(
+                                      "534.34",
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          color: Styles.grayColor,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      getTranslated("family_allowance", context),
+                                      style: const TextStyle(
+                                          fontSize: 14,
+                                          color: Styles.GOLD_COLOR,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    const Text(
+                                      "5675.8",
+                                      style: TextStyle(
+                                          fontSize: 14,
+                                          color: Styles.grayColor,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 24.h,
+                          ),
+                          Text(getTranslated("salary_details", context),
+                              style: const TextStyle(
+                                fontWeight: FontWeight.w700,
+                                fontSize: 16,
+                              )),
+                          Padding(
+                            padding: EdgeInsets.symmetric(vertical: 16.h),
+                            child: Container(
+                                padding: const EdgeInsets.all(
+                                    Dimensions.PADDING_SIZE_EXTRA_LARGE),
+                                decoration: const BoxDecoration(
+                                  color: Styles.FILL_COLOR,
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(15)),
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Text(
+                                      DateTime.now().monthFormat(),
+                                      style: const TextStyle(
+                                          fontSize: 14,
+                                          color: Styles.blackColor,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+
+                                    ///Open Invoice
+                                    Padding(
+                                      padding: EdgeInsets.symmetric(
+                                          vertical: 12.h,
+                                          horizontal:
+                                              Dimensions.PADDING_SIZE_DEFAULT.w),
+                                      child: CustomButton(
+                                        onTap: () => CustomNavigator.push(
+                                            Routes.PDF,
+                                            arguments: ""),
+                                        text: getTranslated("details", context),
+                                      ),
+                                    ),
+                                    ChangeNotifierProvider(
+                                      create: (_) => DownloadProvider(),
+                                      child: Consumer<DownloadProvider>(
+                                          builder: (_, provider, child) {
+                                        return CustomButton(
+                                            textColor: Styles.PRIMARY_COLOR,
+                                            text: getTranslated(
+                                                "download", context),
+                                            onTap: () async {
+                                              if (!provider.downloaded) {
+                                                provider.download(
+                                                    "", "".split("/").last);
+                                              }
+                                            },
+                                            isLoading: provider.isLoading,
+                                            lIconWidget: const Icon(
+                                              Icons.download,
+                                              color: Styles.PRIMARY_COLOR,
+                                            ),
+                                            backgroundColor: Styles.WHITE);
+                                      }),
+                                    ),
+                                  ],
+                                )),
                           ),
                         ],
-                      )),
-                ),
-              ],
-            ),
-          ),
-        ],
+                      ),
+                    ),
+                  ],
+                );
+        }),
       ),
     );
   }
